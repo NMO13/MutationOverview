95cfc200335533bae024ec68d234615829082d21
Martin Ennemoser <martin.ennemoser@gmx.at>
2017-03-22 10:59:01 +0100

    Added stacked barchart

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import * as d3 from 'd3';
 import * as d3_scale from 'd3-scale';
 import * as d3_axis from 'd3-axis';
 import * as d3_selection from 'd3-selection';
+import * as d3_shape from 'd3-shape';
 import * as $ from 'jquery';
 import {HELLO_WORLD} from './language';
 
@@ -34,7 +35,6 @@ export class App {
    * @returns {Promise<App>}
    */
   private build() {
-    this.$node.html(HELLO_WORLD);
 	start1();
     return Promise.resolve(null);
   }
@@ -58,63 +58,140 @@ export function create(parent:Element) {
   return new App(parent);
 }
 
-class BarChart {
-    render(data) {
-       var svg = d3_selection.select("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
-	
-	var x = d3_scale.scaleBand().rangeRound([0, width]).padding(0.1),
-    y = d3_scale.scaleLinear().rangeRound([height, 0]);
-	
-       var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-	
-	x.domain(data.map(function(d) { return d[0]; }));
-  y.domain([0, d3.max(data, function(d) { return d[1]; })]);
+interface Renderable {
+	render(data, params?: Object) : void;
+}
 
-  g.append("g")
-	  .attr("class", "axis axis--x")
-	  .attr("transform", "translate(0," + height + ")")
-	  .call(d3_axis.axisBottom(x));
+class StackChart implements Renderable {
+	render(data, params?: Object) : void {
+		let keys = params[1];
+		let svg = d3_selection.select("#chromosome-ov"),
+		margin = {top: 20, right: 20, bottom: 30, left: 40},
+		width = +svg.attr("width") - margin.left - margin.right,
+		height = +svg.attr("height") - margin.top - margin.bottom,
+		g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		
+		var x = d3_scale.scaleBand()
+			.rangeRound([0, width])
+			.paddingInner(0.05)
+			.align(0.1);
 
-  g.append("g")
-	  .attr("class", "axis axis--y")
-	  .call(d3_axis.axisLeft(y).ticks(10));
-	 
-	g.append("text")
-	  .attr("transform", "rotate(-90)")
-	  .attr("y", 6)
-	  .attr("dy", "0.71em")
-	  .attr("text-anchor", "end")
-	  .text("Mutation Count");
+		var y = d3_scale.scaleLinear()
+			.rangeRound([height, 0]);
 
-  let bars = g.selectAll(".bar")
-	.data(data)
-	.enter().append("rect")
-	  .attr("class", "bar")
-	  .attr("x", function(d) { return x(d[0]); })
-	  .attr("y", function(d) { return y(d[1]); })
-	  .attr("width", x.bandwidth())
-	  .attr("height", function(d) { return height - y(d[1]); });
+		let colorArr = [];
+		for(var i = 0; i < keys.length; i++)
+			colorArr.push(randomColor());
+		var z = d3_scale.scaleOrdinal()
+			.range(colorArr);
+			
+		x.domain(data.map(function(d : History) { return d.chrom; }));
+		y.domain([0, d3.max(data, function(d : History) { return d.total; })]).nice();
+		z.domain(keys);
+		
+		var layers = d3_shape.stack().keys(keys)(data);
+			
+		let rects = g.append("g")
+    .selectAll("g")
+    .data(layers)
+    .enter().append("g")
+      .attr("fill", function(d) 
+	  {
+		  return z(d.key).toString(); 
+		  })
+    .selectAll("rect");
+	
+    rects.data(function(d) 
+	{ 
+	return [d[0], d[1]]; })
+    .enter().append("rect")
+      .attr("x", function(d) 
+	  { 
+		return x(d.data.chrom.toString()) 
+		})
+      .attr("y", function(d) { return y(d[1]); })
+      .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+      .attr("width", x.bandwidth());
 	  
-	  bars.append('title')
-                 .text((d) => d[0] + ' - ' + d[1]);
+	   g.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3_axis.axisBottom(x));
+	}
+}
+
+class BarChart implements Renderable {
+    render(data) : void {
+		let svg = d3_selection.select("#mutation-ov"),
+		margin = {top: 20, right: 20, bottom: 30, left: 40},
+		width = +svg.attr("width") - margin.left - margin.right,
+		height = +svg.attr("height") - margin.top - margin.bottom;
+	
+		let x = d3_scale.scaleBand().rangeRound([0, width]).padding(0.1),
+		y = d3_scale.scaleLinear().rangeRound([height, 0]);
+	
+		let g = svg.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
+		x.domain(data.map(function(d) { return d[0]; }));
+		y.domain([0, d3.max(data, function(d) { return d[1]; })]);
+
+		g.append("g")
+		  .attr("class", "axis axis--x")
+		  .attr("transform", "translate(0," + height + ")")
+		  .call(d3_axis.axisBottom(x));
+
+		g.append("g")
+		  .attr("class", "axis axis--y")
+		  .call(d3_axis.axisLeft(y).ticks(10));
+	 
+		g.append("text")
+		  .attr("transform", "rotate(-90)")
+		  .attr("y", 6)
+		  .attr("dy", "0.71em")
+		  .attr("text-anchor", "end")
+		  .text("Mutation Count");
+
+		let bars = g.selectAll(".bar")
+		  .data(data)
+		  .enter().append("rect")
+	      .attr("class", "bar")
+	      .attr("x", function(d) { return x(d[0]); })
+	      .attr("y", function(d) { return y(d[1]); })
+	      .attr("width", x.bandwidth())
+	      .attr("height", function(d) { return height - y(d[1]); });
+	  
+		bars.append('title')
+          .text((d) => d[0] + ' - ' + d[1]);
 	}
 }
 
 class MutationArray {
     constructor(public array: MutationDatum[]) { }
     groupCountByProperty(propertyName: string) {
-        var hist = {};
-        this.array.map(function (a)
-        {
-            var property = a[propertyName];
+        let hist = {};
+        this.array.map(function (a) {
+            let property = a[propertyName];
             if (property in hist) hist[property]++; else hist[property] = 1;
         });
         return hist;
     }
+	
+	// code from http://codereview.stackexchange.com/questions/37028/grouping-elements-in-array-by-multiple-properties
+	groupByProperties( f )
+	{
+	  let groups = {};
+	  this.array.forEach( function( o )
+	  {
+		let group = JSON.stringify( f(o) );
+		groups[group] = groups[group] || [];
+		groups[group].push( o );  
+	  });
+	  return Object.keys(groups).map( function( group )
+	  {
+		return groups[group]; 
+	  })
+	}
 }
 
 class MutationDatum {
@@ -138,29 +215,107 @@ class JsonDataService implements MutationDataService {
     }
 }
 
+class History {
+	constructor(keys) {
+		for(let k of keys) {
+			this[k] = 0;
+		}
+		this.total = 0;
+		this.chrom = "";
+	}
+	total: number;
+	chrom: string;
+}
+
 function start1() {
-    var obj = new JsonDataService();
-    var data = obj.getData("https://dcc.icgc.org/api/v1/projects/GBM-US/mutations?field=id,mutation,type,chromosome,start,end&size=100&order=desc").then(
+    let obj = new JsonDataService();
+    let data = obj.getData("https://dcc.icgc.org/api/v1/projects/GBM-US/mutations?field=id,mutation,type,chromosome,start,end&size=100&order=desc").then(
         function (returndata) {
             let s: MutationConverter = {
                 convert: function (data) {
-                    var convertedArr = data.hits.map(function (datum) {
+                    let convertedArr = data.hits.map(function (datum) {
                         return new MutationDatum(datum.chromosome, datum.end, datum.id, datum.mutation, datum.start, datum.type);
                     });
                     return new MutationArray(convertedArr);
                 }
-            }
-            var mutationArray = s.convert(returndata);
+            };
+            let mutationArray = s.convert(returndata);
 
-			let dictionary = mutationArray.groupCountByProperty("mutation");
+			let mutations = mutationArray.groupByProperties(function(item)
+			{
+				return [item.mutation];
+			});
 			
 			let arr = [];
-			for(var a in dictionary) {
-				let x: [string, number];
-				x = [a, dictionary[a]];
-				arr.push(x);
-			}
+			arr = mutations.map(function(mutation) {
+				return mutation.length > 0 ? [mutation[0].mutation, mutation.length] : [];
+			});
+			
+			
             let bc = new BarChart();
             bc.render(arr);
+			
+			let chromosomes = mutationArray.groupByProperties(function(item)
+			{
+			  return [item.chromosome];
+			});
+			
+			let keys = arr.map(function(mutation) {
+				return mutation[0];
+			});
+			chromosomes = chromosomes.map(function(ch) {
+				
+				let hist = new History(keys);
+				
+				for(let mutationDatum of ch) {
+					let m = mutationDatum.mutation;
+					if (m in hist) hist[m]++; else hist[m] = 1;
+					hist.chrom = mutationDatum.chromosome;
+					hist.total++;
+				}
+				return hist;
+			});
+			
+			let sc = new StackChart();
+			sc.render(chromosomes, [mutations.length, keys]);
         });   
 }
+
+// Adapted from http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+
+function hue2rgb(p, q, t){
+              if(t < 0) t += 1;
+              if(t > 1) t -= 1;
+              if(t < 1/6) return p + (q - p) * 6 * t;
+              if(t < 1/2) return q;
+              if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+              return p;
+          };
+		  
+var randomColor = (function(){
+  var golden_ratio_conjugate = 0.618033988749895;
+  var h = Math.random();
+
+  var hslToRgb = function (h, s, l){
+      var r, g, b;
+
+      if(s == 0){
+          r = g = b = l; // achromatic
+      }else{
+          
+
+          var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+          var p = 2 * l - q;
+          r = hue2rgb(p, q, h + 1/3);
+          g = hue2rgb(p, q, h);
+          b = hue2rgb(p, q, h - 1/3);
+      }
+
+      return '#'+Math.round(r * 255).toString(16)+Math.round(g * 255).toString(16)+Math.round(b * 255).toString(16);
+  };
+	return function(){
+		h += golden_ratio_conjugate;
+		h %= 1;
+		return hslToRgb(h, 0.5, 0.60);
+	  };
+	})();
